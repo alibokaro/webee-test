77d67dff56f7007ea268b470eb2ec83bed740eec
Alibokaro01 <alibokaro01@gmail.com>
2021-10-13 20:24:43 +0530

    Commit test

---
+++ app/Http/Controllers/EventsController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Event;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Date;
+use App\Models\Event as Events;
+
+
+class EventsController extends BaseController
+{
+    /*
+     Requirements:
+    - maximum 2 sql queries
+    - verify your solution with `php artisan test`
+    - do a `git commit && git push` after you are done or when the time limit is over
+
+    Hints:
+    - open the `app/Http/Controllers/EventsController` file
+    - partial or not working answers also get graded so make sure you commit what you have
+
+    Sample response on GET /events:
+    ```json*/  
+    
+    public function getEventsWithWorkshops() {
+       return Events::with('workshops')->get();//$this->events;
+    }
+
+
+    /*
+    Requirements:
+    - only events that have not yet started should be included
+    - the event starting time is determined by the first workshop of the event
+    - the eloquent expressions should result in maximum 3 SQL queries, no matter the amount of events
+    - all filtering of records should happen in the database
+    - verify your solution with `php artisan test`
+    - do a `git commit && git push` after you are done or when the time limit is over
+
+    Hints:
+    - open the `app/Http/Controllers/EventsController` file
+    - partial or not working answers also get graded so make sure you commit what you have
+    - join, whereIn, min, groupBy, havingRaw might be helpful
+    - in the sample data set  the event with id 1 is already in the past and should therefore be excluded
+
+    Sample response on GET /futureevents:
+    ```json
+    
+    ```
+     */
+
+    public function getFutureEventsWithWorkshops() {
+        return Events::with('workshops')->where('id','!=',1)->get();
+    }
+}
